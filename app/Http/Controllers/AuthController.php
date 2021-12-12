b3f3285e0aefeed062e401ae9047b04f23ed95d8
Le Bui <lebui@Les-MacBook-Air.local>
2021-12-12 16:28:42 +0700

    make it better

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,32 +11,35 @@ use DB;
 use App\Http\Requests;
 use Session;
 use Illuminate\Support\Facades\Redirect;
-if(!isset($_SESSION))
-{
+
+if (!isset($_SESSION)) {
     session_start();
 }
 
 class AuthController extends Controller
 {
-    public function  login(Request $request){
+    public function  login(Request $request)
+    {
         return view('user.login');
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         if ($request->isMethod('post')) {
-            
         }
         return view('user.register');
     }
     public function store(Request $request)
     {
+        $request->flash();
         $request->validate(
             [
                 'phone' => 'required|min:11|numeric|regex:/(01)[0-9]{9}/'
             ],
             [
                 'phone' => '正しい電話フォーマットを入力してください'
-            ]);
+            ]
+        );
         $restauran = new User;
         $restauran->user_name = $request->user_name;
         $restauran->email = $request->email;
@@ -47,26 +50,28 @@ class AuthController extends Controller
         $number_of_users = User::where('isrestauran', 0)->count();
         $number_of_restaurans = User::where('isrestauran', 1)->count();
         $number_of_posts = Post::count();
-            $number_of_tags = Tag::count();
-            $users = User::all();
-            $posts = Post::all();
-            $tags = Tag::all();
-            $tags = $tags->SortByDesc('tag_id');
+        $number_of_tags = Tag::count();
+        $users = User::all();
+        $posts = Post::all();
+        $tags = Tag::all();
+        $tags = $tags->SortByDesc('tag_id');
         return view('admin.home')
-        ->with(compact('number_of_users', $number_of_users))
-        ->with(compact('number_of_restaurans', $number_of_restaurans))
-        ->with(compact('number_of_posts', $number_of_posts))
-                ->with(compact('number_of_tags', $number_of_tags))
-                ->with(compact('users', $users))
-                ->with(compact('posts', $posts))
-                ->with(compact('tags', $tags));
+            ->with(compact('number_of_users', $number_of_users))
+            ->with(compact('number_of_restaurans', $number_of_restaurans))
+            ->with(compact('number_of_posts', $number_of_posts))
+            ->with(compact('number_of_tags', $number_of_tags))
+            ->with(compact('users', $users))
+            ->with(compact('posts', $posts))
+            ->with(compact('tags', $tags));
     }
 
-    public function profile(Request $request){
+    public function profile(Request $request)
+    {
         return view('user_profile');
     }
 
-    public function change_pass(Request $request){
+    public function change_pass(Request $request)
+    {
         return view('user.change_pass');
     }
 }
